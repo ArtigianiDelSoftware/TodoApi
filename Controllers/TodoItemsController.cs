1b4eaf65ade9d3691379db2b57ebe9c98992d53a
Luca Perico <pericoluca1993@gmail.com>
2021-05-05 11:32:56 +0200

    Move fake error

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -26,7 +26,7 @@ namespace TodoApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
         {
-            return await _context.TodoItems.ToListAsync();
+            return Forbid();
         }
 
         // GET: api/TodoItems/5
@@ -40,7 +40,7 @@ namespace TodoApi.Controllers
                 return NotFound();
             }
 
-            return Forbid();
+            return todoItem;
         }
 
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
